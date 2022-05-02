0f07e4a949838385c100203bc363ee98cb833925
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-05-02 19:03:20 -0300

    pacientes atualizando

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,9 @@ const routes = express.Router();
 
 
 routes.post("/pacientes", pacientesController.cadastrar);
+routes.get("/pacientes", pacientesController.listarPacientes);
+routes.get("/pacientes/:id", pacientesController.buscarPeloId);
+routes.delete("/pacientes/:id/deletar", pacientesController.deletarPaciente);
+routes.put("/pacientes/:id/atualizar", pacientesController.atualizarPaciente);
 
 module.exports = routes;
