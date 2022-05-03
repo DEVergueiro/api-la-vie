a367703d2064decf58d10e801b21fc1032ba9fe7
devergueiro <100455051+ntvergueiro@users.noreply.github.com>
2022-05-02 22:09:25 -0300

    psicologos finalizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const pacientesController = require("../controllers/controllerPacientes")
+const pacientesController = require("../controllers/controllerPacientes");
+const psicologosController = require("../controllers/controllerPsicologos");
 
 const routes = express.Router();
 
@@ -7,7 +8,13 @@ const routes = express.Router();
 routes.post("/pacientes", pacientesController.cadastrar);
 routes.get("/pacientes", pacientesController.listarPacientes);
 routes.get("/pacientes/:id", pacientesController.buscarPeloId);
-routes.delete("/pacientes/:id/deletar", pacientesController.deletarPaciente);
-routes.put("/pacientes/:id/atualizar", pacientesController.atualizarPaciente);
+routes.delete("/pacientes/:id", pacientesController.deletarPaciente);
+routes.put("/pacientes/:id", pacientesController.atualizarPaciente);
+
+routes.get("/psicologos", psicologosController.listarPsicologos);
+routes.get("/psicologos/:id", psicologosController.listarPsicologosId);
+routes.delete("/psicologos/:id", psicologosController.deletarPsicologo)
+routes.post("/psicologos/", psicologosController.cadastrarPsicologo)
+routes.put("/psicologos/:id", psicologosController.atualizarPsicologo)
 
 module.exports = routes;
