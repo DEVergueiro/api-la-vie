3422ff03ebf003e5ffe6bd6501b07ca7e8b30fae
Junior Sott <sottjr@gmail.com>
2022-05-07 15:07:22 +0200

    Acrescentando validação via ID por parametros

--- src/validators/atendimentosValidation.js
+++ src/validators/atendimentosValidation.js
@@ -3,6 +3,9 @@ const { validate, Joi} = require("express-validation")
 
 
 module.exports = validate({
+    params: Joi.object({
+        id: Joi.number().required()
+    }),
     body: Joi.object({
         paciente_id: Joi.required(),
         data_atendimento: Joi.string().required(),
--- src/validators/psicologosValidation.js
+++ src/validators/psicologosValidation.js
@@ -2,6 +2,9 @@ const { validate, Joi} = require("express-validation")
 
 
 module.exports = validate({
+    params: Joi.object({
+        id: Joi.number().required()
+    }),
     body: Joi.object({
         nome: Joi.string().required(),
         email: Joi.string().email().required(),
